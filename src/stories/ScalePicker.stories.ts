32dcea9319c4b54458688eede970bc0b73030e51
James Coyle <jamescoyle1989@gmail.com>
2023-09-10 00:05:04 -0400

    Added ScalePicker stories

---
+++ src/stories/ScalePicker.stories.ts
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { withActions } from '@storybook/addon-actions/decorator';
+import { ScalePicker } from '../scale-picker';
+import { ScaleTemplate } from 'shimi';
+
+
+const meta: Meta<typeof ScalePicker> = {
+    title: 'ScalePicker',
+    tags: ['autodocs'],
+    render: (args: any) => {
+        const output = new ScalePicker();
+        for (const prop in args)
+            (output as any)[prop] = args[prop];
+        return output;
+    },
+    argTypes: {
+        scaleTemplates: {
+            description: 'A collection of shimi.ScaleTemplate objects. This defines which scale types that the user is allowed to select from in the first dropdown.'
+        }
+    },
+    parameters: {
+        actions: {
+            handles: ['scale-change']
+        }
+    },
+    decorators: [withActions]
+};
+export default meta;
+
+
+type Story = StoryObj<ScalePicker>;
+
+export const Primary: Story = {
+    args: {
+        scaleTemplates: [ScaleTemplate.major, ScaleTemplate.naturalMinor]
+    }
+};
+
+export const ScaleTemplatesNotSet: Story = {
+    args: {
+    }
+};
