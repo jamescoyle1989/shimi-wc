5400a1708e5ee8bd2cc2f3f6c8c0338dc4484c86
James Coyle <jamescoyle1989@gmail.com>
2023-09-09 23:42:39 -0400

    Added documentation for in & out port picker stories

--- src/stories/OutPortPicker.stories.ts
+++ src/stories/OutPortPicker.stories.ts
@@ -13,6 +13,22 @@ const meta: Meta<typeof OutPortPicker> = {
             (output as any)[prop] = args[prop];
         return output;
     },
+    argTypes: {
+        midiAccess: {
+            control: 'none',
+            description: 'Holds a reference to a shimi.MidiAccess object, used to fetch MIDI-Out ports that can be connected to. This also enables the control to listen for newly added/removed ports and update itself accordingly. This property must be set for the component to be in any way useful.'
+        },
+        port: {
+            control: {
+                type: 'text'
+            },
+            description: 'The name of the selected port. If this is set to a value which doesn\'t correspond to an available port name, then the component will simply show no port selected. This property will be updated when the user selects new dropdown values, as well as a port-change event being fired.'
+        },
+        midiOut: {
+            control: 'none',
+            description: 'Holds a reference to a shimi.MidiOut object, which gets updated with the selected port when changed. This is optional to set, and is really provided just as a convenience over listening for the port-change event to update the MidiOut.'
+        }
+    },
     parameters: {
         actions: {
             handles: ['port-change']
@@ -43,4 +59,4 @@ export const PortPresetToInvalidName: Story = {
         midiAccess: newMockMidiAccess(),
         port: 'Made Up Port'
     }
-}
+};
