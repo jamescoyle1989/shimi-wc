c7dd43b3425aa0457c16f31038cac88347f9b28b
James Coyle <jamescoyle1989@gmail.com>
2023-09-16 17:14:09 -0400

    Moved storybook static pages to /docs

--- storybook-static/assets/preview-375d4b62.js
+++
@@ -1,2 +0,0 @@
-import{_ as e}from"./iframe-55146820.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-3PUGWF3O-8270d5e0.js"),["./DocsRenderer-3PUGWF3O-8270d5e0.js","./iframe-55146820.js","./index-e33fe827.js","./_commonjsHelpers-725317a4.js","./index-d37d4223.js","./index-d38538b0.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{a as parameters};
-//# sourceMappingURL=preview-375d4b62.js.map
